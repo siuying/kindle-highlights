e7d58847fd49f74a47b6d2a67be3024592b7ad33
Francis Chong <francis@ignition.hk>
2012-11-12 12:19:15 +0800

    clear cookies before signin

--- lib/kindle-highlights.rb
+++ lib/kindle-highlights.rb
@@ -9,6 +9,7 @@ class KindleHighlight
   def initialize(email_address, password)
     @highlights = Array.new
     @agent = Mechanize.new
+    @agent.cookie_jar.clear!
     @fetched = false
 
     page = @agent.get("https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F&pageId=amzn_kindle&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&openid.assoc_handle=amzn_kindle&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select")
