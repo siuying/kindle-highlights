473eb321e72959b71878fa898b48216862dac5a0
Francis Chong <francis@ignition.hk>
2012-11-12 13:09:38 +0800

    use Mac Safari as user agent

--- lib/kindle-highlights.rb
+++ lib/kindle-highlights.rb
@@ -9,10 +9,10 @@ class KindleHighlight
   def initialize(email_address, password)
     @highlights = Array.new
     @agent = Mechanize.new
-    @agent.cookie_jar.clear!
+    @agent.user_agent_alias = 'Mac Safari'
     @fetched = false
 
-    page = @agent.get("https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F&pageId=amzn_kindle&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.pape.max_auth_age=0&openid.assoc_handle=amzn_kindle&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select")
+    page = @agent.get("https://www.amazon.com/ap/signin?openid.assoc_handle=amzn_kindle&openid.pape.max_auth_age=0&pageId=amzn_kindle&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.return_to=https%3A%2F%2Fkindle.amazon.com%3A443%2Fauthenticate%2Flogin_callback%3Fwctx%3D%252F")
     @amazon_form = page.form('signIn')
     @amazon_form.email = email_address
     @amazon_form.password = password
